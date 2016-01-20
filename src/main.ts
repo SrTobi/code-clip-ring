0d672ebe97fbffaabce92a9e536b786f3ca1ad7d
SrTobi <code.databyte@gmail.com>
2016-01-20 21:05:38 +0100

    fixed ring command registration

--- src/main.ts
+++ src/main.ts
@@ -5,24 +5,24 @@ import * as ringcmd from './ring_commands';
 
 export function activate(context: vscode.ExtensionContext) {
 
-	console.log('Extension "code-clip-registry" is now active!'); 
+	console.log('Extension "code-clip-ring" is now active!'); 
 
     // Ring commands
-	var disposable = vscode.commands.registerCommand('clipreg.copyToRing', ringcmd.copyToRing);
+	var disposable = vscode.commands.registerCommand('clipring.copyToRing', ringcmd.copyToRing);
 	context.subscriptions.push(disposable);
     
-	disposable = vscode.commands.registerCommand('clipreg.cutToRing', ringcmd.cutToRing);
+	disposable = vscode.commands.registerCommand('clipring.cutToRing', ringcmd.cutToRing);
 	context.subscriptions.push(disposable);
     
-	disposable = vscode.commands.registerCommand('clipreg.pasteRingItem', ringcmd.pasteRingItem);
+	disposable = vscode.commands.registerCommand('clipring.pasteRingItem', ringcmd.pasteRingItem);
 	context.subscriptions.push(disposable);
     
-	disposable = vscode.commands.registerCommand('clipreg.selectAndPasteRingItem', ringcmd.selectAndPasteRingItem);
+	disposable = vscode.commands.registerCommand('clipring.selectAndPasteRingItem', ringcmd.selectAndPasteRingItem);
 	context.subscriptions.push(disposable);
     
-	disposable = vscode.commands.registerCommand('clipreg.removeAllRingItems', ringcmd.removeAllRingItem);
+	disposable = vscode.commands.registerCommand('clipring.removeAllRingItems', ringcmd.removeAllRingItem);
 	context.subscriptions.push(disposable);
     
-	disposable = vscode.commands.registerCommand('clipreg.removeRingItem', ringcmd.removeRingItems);
+	disposable = vscode.commands.registerCommand('clipring.removeRingItem', ringcmd.removeRingItems);
 	context.subscriptions.push(disposable);
 }
